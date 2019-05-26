613c4722a2dc9381bcc12648da9b517eca8a6578
GENYA <t6589989@gmail.com>
2019-05-26 15:40:14 +0800

    create TestChat

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -15,9 +15,18 @@ namespace PersonalWeb.Hubs
         {
 
 
-            var content = $"<div class='float-left'>使用者：{user}</div>" +
-                          $"<div class='float-right'>{DateTime.Now.ToShortDateString()}&nbsp{DateTime.Now.ToLongTimeString()}</div><br />" +
-                          $"<div>{message}</div>";
+            var content = $"<div class='container'>" +
+                            $"<div class='row'>"+
+                                $"<div class='col-2'>&nbsp;</div>"+
+                                $"<div class='col-8 chatbox'>"+
+                                    $"<div class='float-left'>使用者：{user}</div>" +
+                                    $"<div class='float-right'>{DateTime.Now.ToShortDateString()}&nbsp{DateTime.Now.ToLongTimeString()}</div><br />" +
+                                    $"<div class='col-12'><hr /></div>" +
+                                    $"<div>{message}</div>"+
+                                $"</div>" +
+                            $"</div>" +
+                          $"</div>" +
+                          $"<br />";
 
             await Clients.All.SendAsync("ReceiveMessage", content);
         }
